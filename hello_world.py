bfddd2f7004285cc9e08c4e2cea802eaa06b9ed6
Khyati Ghatalia <37407336+khyati-ghatalia@users.noreply.github.com>
2020-08-21 16:10:16 +0100

    Added 2 programs

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+#This is my first python program
+
+#Priniting hello world
+print("hello World!")
+
+#Concatenating strings
+
+print("hello" + " " + "world!")
+
+#printing hello 10 times
+
+print("hello" * 10)
---
+++ play_with_strings.py
@@ -0,0 +1,17 @@
+#This is my second python program
+# I am playing around with string operations
+
+#Defing a string variable
+string_hello = "This is Khyatis program"
+
+#Printing the string
+print("%s" %string_hello)
+
+#Printing the length of the string
+print(len(string_hello))
+
+#Finding the first instance of a string in a string
+print(string_hello.index("K"))
+
+#Reverse of a string
+print(string_hello[::-1])
