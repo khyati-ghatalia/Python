30a0ed5a343c4796a8ba1e32cffc435051426663
Khyati Ghatalia <khyati.86@gmail.com>
2020-08-25 22:02:09 +0100

    Function examples file

---
+++ func_examples.py
@@ -0,0 +1,9 @@
+#The code below is the definition of the function
+#All I have done here is to create a simple function 
+# which prints "This is my first func"
+
+def myfirstfunc():
+    print("This is my first func")
+
+#The below code calls myfirstfunc()
+myfirstfunc()
